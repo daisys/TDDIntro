9915ea60670e4b551bc38efba9f9218efcf07b78
Daisy Shih <ilovecanada611@hotmail.com>
2014-09-25 03:38:13 -0700

    Revert back to origin

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,13 +2,6 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i<0){
-            throw new IllegalArgumentException();
-        }
-
-        if(i == 3){
-            return 6;
-        }
         return -1;
     }
 }
