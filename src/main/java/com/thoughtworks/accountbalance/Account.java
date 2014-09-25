eaebc2fd316c9ef48114d5728523d5d5900dcf08
Daisy Shih <ilovecanada611@hotmail.com>
2014-09-25 04:05:01 -0700

    Create initial test. Failed so created new class Account and constructor And new method deposit()

---
+++ src/main/java/com/thoughtworks/accountbalance/Account.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.accountbalance;
+
+/**
+ * Created by daisyshih on 2014-09-25.
+ */
+public class Account {
+    private double balance;
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public int deposit(double i) {
+        return 150;
+    }
+}
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -1,12 +1,22 @@
 package com.thoughtworks.accountbalance;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
+
+    private final Account account = new Account(100);
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney() {
+        assertThat(account.deposit(50), is(150));
+
+    }
+
+
 //
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
